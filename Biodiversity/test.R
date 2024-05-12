ba295227fb004c2d03ee782834abb9fdab3b0643
Zzf <zfzhang97@163.com>
2024-05-12 15:49:48 +0800

    master

--- Biodiversity/test.R
+++ Biodiversity/test.R
@@ -1,15 +1,32 @@
-
 library(ggplot2)
-# 加载数据集
-data("iris")
 
-# 计算每种鸢尾花花瓣平均长度
-avg_petal_length <- aggregate(Petal.Length ~ Species, data = iris, mean)
+# 创建数据
+set.seed(123)  # 设置随机数种子以确保结果可重现
+data <- data.frame(
+  Group = rep(c("A", "B"), each = 50),
+  Value = c(rnorm(50, mean = 100, sd = 10), rnorm(50, mean = 110, sd = 10))
+)
+# 创建散点图
+ggplot(data, aes(x = Group, y = Value, color = Group)) +
+  geom_point() +
+  ggtitle("散点图") +
+  theme_minimal()
 
-# 绘制条形图
-ggplot(avg_petal_length, aes(x = Species, y = Petal.Length, fill = Species)) +
-  geom_bar(stat = "identity", color = "black") +
-  labs(title = "不同类型的鸢尾花的平均花瓣长度",
-       x = "鸢尾花种类",
-       y = "平均花瓣长度 (cm)") +
+# 创建箱线图
+ggplot(data, aes(x = Group, y = Value, fill = Group)) +
+  geom_boxplot() +
+  ggtitle("箱线图") +
+  theme_minimal()
+
+# 创建直方图
+ggplot(data, aes(x = Value, fill = Group)) +
+  geom_histogram(position = "identity", alpha = 0.5, bins = 30) +
+  facet_wrap(~ Group) +
+  ggtitle("直方图") +
+  theme_minimal()
+
+# 创建密度图
+ggplot(data, aes(x = Value, fill = Group, color = Group)) +
+  geom_density(alpha = 0.5) +
+  ggtitle("密度图") +
   theme_minimal()
