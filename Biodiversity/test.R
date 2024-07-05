43f6d42166906a2cbe99722c239a79f4b799309c
Zzf <zfzhang97@163.com>
2024-07-05 13:51:20 +0800

    commit on master

--- Biodiversity/test.R
+++ Biodiversity/test.R
@@ -1,32 +1,33 @@
-library(ggplot2)
+library(BIEN)
+library(ape) #Package for working with phylogenies in R
+library(maps) #Useful for making quick maps of occurrences
 
-# 创建数据
-set.seed(123)  # 设置随机数种子以确保结果可重现
-data <- data.frame(
-  Group = rep(c("A", "B"), each = 50),
-  Value = c(rnorm(50, mean = 100, sd = 10), rnorm(50, mean = 110, sd = 10))
-)
-# 创建散点图
-ggplot(data, aes(x = Group, y = Value, color = Group)) +
-  geom_point() +
-  ggtitle("散点图") +
-  theme_minimal()
+library(sf)
+vignette("BIEN")
+Xanthium_strumarium <- BIEN_occurrence_species(species = "Xanthium strumarium")
+str(Xanthium_strumarium)
+head(Xanthium_strumarium)
+LUQUILLO_full <- BIEN_plot_name(plot.name = "LUQUILLO",
+                                cultivated = TRUE,
+                                all.taxonomy = TRUE,
+                                native.status = TRUE,
+                                political.boundaries = TRUE,
+                                all.metadata = TRUE)
+library(rgbif)
+help.start()
+library(ALA4R)
+occurrences(taxon="data_resource_uid:dr356",record_count_only=TRUE)
+dataset_doi('10.15468/igasai')
+ala_config()
+layers = c('cl22','cl23','el773')
+pnts = c(-23.1,149.1)
+intersect_points(pnts,layers)
 
-# 创建箱线图
-ggplot(data, aes(x = Group, y = Value, fill = Group)) +
-  geom_boxplot() +
-  ggtitle("箱线图") +
-  theme_minimal()
+885.34+2278.8
+2278.8
 
-# 创建直方图
-ggplot(data, aes(x = Value, fill = Group)) +
-  geom_histogram(position = "identity", alpha = 0.5, bins = 30) +
-  facet_wrap(~ Group) +
-  ggtitle("直方图") +
-  theme_minimal()
-
-# 创建密度图
-ggplot(data, aes(x = Value, fill = Group, color = Group)) +
-  geom_density(alpha = 0.5) +
-  ggtitle("密度图") +
-  theme_minimal()
+install.packages('FD')
+library(FD)
+head(dummy)
+aa <- dbFD(dummy$trait, dummy$abun)
+summary(aa)
